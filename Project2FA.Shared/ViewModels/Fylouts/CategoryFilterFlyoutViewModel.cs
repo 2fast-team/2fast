b97d709397eefa56fc8b408cd557e97b036363e7
Jan Philipp Weber <10946850+jp-weber@users.noreply.github.com>
2025-08-09 21:00:03 +0200

    chore: nuget packages updated

--- Project2FA.Shared/ViewModels/Fylouts/CategoryFilterFlyoutViewModel.cs
+++ Project2FA.Shared/ViewModels/Fylouts/CategoryFilterFlyoutViewModel.cs
@@ -1,8 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using UNOversal.Ioc;
